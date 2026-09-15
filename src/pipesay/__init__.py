5b7959b585da74ec70704996f5d087c034815fab
OrO-c <nightgpt@outlook.com>
2026-09-15 22:15:44 +0800

    chore: 使用更好的版本发布

--- src/pipesay/__init__.py
+++ src/pipesay/__init__.py
@@ -1,9 +1,14 @@
 # src/pipesay/__init__.py
+from importlib.metadata import PackageNotFoundError, version
+
 from pipesay.core.pipeline import Pipeline
 from pipesay.core.registry import register
 from pipesay.fetcher.fetcher import Fetcher
 from pipesay.outputers.base import Outputer
 from pipesay.processor.base import Processor
 
-__version__ = "0.0.1"
+try:
+    __version__ = version("pipesay")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 __all__ = ["Fetcher", "Outputer", "Pipeline", "Processor", "register"]
